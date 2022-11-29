3aaad0e5beab8c6e0bbc5039ee4296ab4fe7e5ad
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-11-30 00:04:15 +0530

    :beers: add model

---
+++ utils/captioner.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+
+class SeqEmbedding(tf.keras.layers.Layer):
+  def __init__(self, vocab_size, max_length, depth):
+    super().__init__()
+    self.pos_embedding = tf.keras.layers.Embedding(input_dim=max_length, output_dim=depth)
+
+    self.token_embedding = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=depth,
+        mask_zero=True)
+
+    self.add = tf.keras.layers.Add()
+
+  def call(self, seq):
+    seq = self.token_embedding(seq) # (batch, seq, depth)
+
+    x = tf.range(tf.shape(seq)[1])  # (seq)
+    x = x[tf.newaxis, :]  # (1, seq)
+    x = self.pos_embedding(x)  # (1, seq, depth)
+
+    return self.add([seq,x])
+
+class CausalSelfAttention(tf.keras.layers.Layer):
+  def __init__(self, **kwargs):
+    super().__init__()
+    self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+    # Use Add instead of + so the keras mask propagates through.
+    self.add = tf.keras.layers.Add()
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x):
+    attn = self.mha(query=x, value=x,
+                    use_causal_mask=True)
+    x = self.add([x, attn])
+    return self.layernorm(x)
+
+class CrossAttention(tf.keras.layers.Layer):
+  def __init__(self,**kwargs):
+    super().__init__()
+    self.mha = tf.keras.layers.MultiHeadAttention(**kwargs)
+    self.add = tf.keras.layers.Add()
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x, y, **kwargs):
+    attn, attention_scores = self.mha(
+             query=x, value=y,
+             return_attention_scores=True)
+
+    self.last_attention_scores = attention_scores
+
+    x = self.add([x, attn])
+    return self.layernorm(x)
+
+class FeedForward(tf.keras.layers.Layer):
+  def __init__(self, units, dropout_rate=0.1):
+    super().__init__()
+    self.seq = tf.keras.Sequential([
+        tf.keras.layers.Dense(units=2*units, activation='relu'),
+        tf.keras.layers.Dense(units=units),
+        tf.keras.layers.Dropout(rate=dropout_rate),
+    ])
+
+    self.layernorm = tf.keras.layers.LayerNormalization()
+
+  def call(self, x):
+    x = x + self.seq(x)
+    return self.layernorm(x)
+
+class DecoderLayer(tf.keras.layers.Layer):
+  def __init__(self, units, num_heads=1, dropout_rate=0.1):
+    super().__init__()
+
+    self.self_attention = CausalSelfAttention(num_heads=num_heads,
+                                              key_dim=units,
+                                              dropout=dropout_rate)
+    self.cross_attention = CrossAttention(num_heads=num_heads,
+                                          key_dim=units,
+                                          dropout=dropout_rate)
+    self.ff = FeedForward(units=units, dropout_rate=dropout_rate)
+
+
+  def call(self, inputs, training=False):
+    in_seq, out_seq = inputs
+
+    # Text input
+    out_seq = self.self_attention(out_seq)
+
+    out_seq = self.cross_attention(out_seq, in_seq)
+
+    self.last_attention_scores = self.cross_attention.last_attention_scores
+
+    out_seq = self.ff(out_seq)
+
+    return out_seq
+
+class Captioner(tf.keras.Model):
+  @classmethod
+  def add_method(cls, fun):
+    setattr(cls, fun.__name__, fun)
+    return fun
+
+  def __init__(self, tokenizer, feature_extractor, output_layer, num_layers=1,
+               units=256, max_length=50, num_heads=1, dropout_rate=0.1):
+    super().__init__()
+    self.feature_extractor = feature_extractor
+    self.tokenizer = tokenizer
+    self.word_to_index = tf.keras.layers.StringLookup(
+        mask_token="",
+        vocabulary=tokenizer.get_vocabulary())
+    self.index_to_word = tf.keras.layers.StringLookup(
+        mask_token="",
+        vocabulary=tokenizer.get_vocabulary(),
+        invert=True)
+
+    self.seq_embedding = SeqEmbedding(
+        vocab_size=tokenizer.vocabulary_size(),
+        depth=units,
+        max_length=max_length)
+
+    self.decoder_layers = [
+        DecoderLayer(units, num_heads=num_heads, dropout_rate=dropout_rate)
+        for n in range(num_layers)]
+
+    self.output_layer = output_layer
+
+IMAGE_SHAPE=(224, 224, 3)
+mobilenet = tf.keras.applications.MobileNetV3Small(
+    input_shape=IMAGE_SHAPE,
+    include_top=False,
+    include_preprocessing=True)
+mobilenet.trainable=False
